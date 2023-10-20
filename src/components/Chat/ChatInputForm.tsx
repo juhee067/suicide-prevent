10262c1abd8c2300f3a162611c8dedd070033a7a
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-10-20 10:40:36 +0900

    refactor:isUser 로그인 유저 속성오류 수정

--- src/components/Chat/ChatInputForm.tsx
+++ src/components/Chat/ChatInputForm.tsx
@@ -1,5 +1,6 @@
 // ChatInputForm.tsx
 
+import { onAuthStateChanged, User } from "firebase/auth";
 import firebase from "firebase/compat";
 import {
   addDoc,
@@ -11,11 +12,12 @@ import {
   serverTimestamp,
   Timestamp,
 } from "firebase/firestore";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-import { db } from "../../firebaseConfig";
+import { auth, db } from "../../firebaseConfig";
 import { formatDateTime } from "../../module/postTime";
 
 const InputContainer = styled.div`
@@ -45,32 +47,56 @@ const SendButton = styled.button`
 
 const ChatInputForm = () => {
   const [message, setMessage] = useState("");
-
+  const navigate = useNavigate();
   const messagesRef = collection(db, "messages");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
   };
   const userData = useSelector((state: { userLoginDataSlice: any }) => state.userLoginDataSlice);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  // ...
+
+  useEffect(() => {
+    // 사용자의 로그인 상태를 확인합니다.
+    const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
+      if (user) {
+        setIsUserLoggedIn(true);
+      } else {
+        setIsUserLoggedIn(false);
+      }
+    });
+
+    return () => {
+      // 컴포넌트가 unmount될 때 구독을 정리합니다.
+      unsubscribe();
+    };
+  }, [auth]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    onAuthStateChanged(auth, async (user: User | null) => {
+      if (isUserLoggedIn) {
+        if (message.trim() !== "") {
+          try {
+            // 새로운 메시지를 Firestore에 추가
+            await addDoc(messagesRef, {
+              text: message,
+              createdAt: formatDateTime(new Date()).toLocaleString(),
+              nickname: userData.nickName,
+            });
 
-    if (message.trim() !== "") {
-      try {
-        // 새로운 메시지를 Firestore에 추가
-        await addDoc(messagesRef, {
-          text: message,
-          createdAt: formatDateTime(new Date()).toLocaleString(),
-          nickname: userData.nickName,
-        });
-
-        // 입력 필드 초기화
-        setMessage("");
-      } catch (error) {
-        console.error("Error adding message: ", error);
+            // 입력 필드 초기화
+            setMessage("");
+          } catch (error) {
+            console.error("Error adding message: ", error);
+          }
+        }
+      } else {
+        alert("로그인이 필요합니다");
+        navigate("/auth/signIn");
       }
-    }
+    });
   };
 
   return (
--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -16,15 +16,15 @@ const MessageContainer = styled(FlexColumnDiv)`
   margin: 30px auto;
 `;
 
-const Message = styled(FlexRowDiv)<{ isUser: boolean }>`
-  flex-direction: ${(props) => (props.isUser ? "row" : "row-reverse")};
-  align-self: ${(props) => (props.isUser ? "flex-end" : "flex-start")};
+const Message = styled(FlexRowDiv)<{ $isUser: boolean }>`
+  flex-direction: ${(props) => (props.$isUser ? "row" : "row-reverse")};
+  align-self: ${(props) => (props.$isUser ? "flex-end" : "flex-start")};
   align-items: flex-end;
   gap: 10px;
 `;
 
-const UserBox = styled(FlexColumnDiv)<{ isUser: boolean }>`
-  align-items: ${(props) => (props.isUser ? "flex-end" : "flex-start")};
+const UserBox = styled(FlexColumnDiv)<{ $isUser: boolean }>`
+  align-items: ${(props) => (props.$isUser ? "flex-end" : "flex-start")};
 `;
 
 const Nickname = styled(Caption)`
@@ -33,12 +33,12 @@ const Nickname = styled(Caption)`
   font-weight: 400;
 `;
 
-const Content = styled.div<{ isUser: boolean }>`
+const Content = styled.div<{ $isUser: boolean }>`
   padding: 10px;
-  border: 2px solid ${(props) => (props.isUser ? "#000000" : "#ffffff")};
-  background-color: ${(props) => (props.isUser ? "#fff" : "#000")};
-  border-radius: ${(props) => (props.isUser ? "12px 12px 0 12px " : "12px 12px  12px 0")};
-  color: ${(props) => (props.isUser ? "#000" : "#fff")};
+  border: 2px solid ${($isUser) => ($isUser ? "#000000" : "#ffffff")};
+  background-color: ${($isUser) => ($isUser ? "#fff" : "#000")};
+  border-radius: ${($isUser) => ($isUser ? "12px 12px 0 12px " : "12px 12px  12px 0")};
+  color: ${($isUser) => ($isUser ? "#000" : "#fff")};
 `;
 
 const TimeBox = styled.div``;
@@ -52,13 +52,13 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ message, user, currentUser, t
   const isCurrentUser = user === currentUser;
   return (
     <MessageContainer>
-      <Message isUser={isCurrentUser}>
+      <Message $isUser={isCurrentUser}>
         <TimeBox>
           <Time>{time}</Time>
         </TimeBox>
-        <UserBox isUser={isCurrentUser}>
+        <UserBox $isUser={isCurrentUser}>
           <Nickname>{isCurrentUser ? currentUser : user}</Nickname>
-          <Content isUser={isCurrentUser}>{message}</Content>
+          <Content $isUser={isCurrentUser}>{message}</Content>
         </UserBox>
       </Message>
     </MessageContainer>
